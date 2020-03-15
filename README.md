29c68f964980ce63d0928f89a651ac45a01acbd5
Cezary Piątek <cezarypiatek@live.com>
2020-03-15 10:31:51 +0100

    Add readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# CSharpExtensions
+
+
+
+## Analyzers
+
+|Rule|Description| Related attributes|Documentation|
+|----|-----------|-------------------|-------------|
+|CSE001|Required properties initialization| `[InitRequired]` | [link](https://cezarypiatek.github.io/post/immutable-types-with-roslyn/#convenient-initialization) |
+|CSE002|InitOnly member modification |`[InitOnly]`| [link](https://cezarypiatek.github.io/post/immutable-types-with-roslyn/#full-immutability) |
+|CSE003|Type should have the same fields as twin type| `[TwinType]` ||
--- src/CSharpExtensions.Annotations/InitOnlyAttribute.cs
+++ src/CSharpExtensions.Annotations/InitOnlyAttribute.cs
@@ -5,7 +5,7 @@ namespace SmartAnalyzers.CSharpExtensions.Annotations
     /// <summary>
     /// Specifies that the member this attribute is bound to
     /// is readonly and should be obligatorily initialized through initialization block.
-    /// Applied on the type level enforces this rule for all members (fields and properties)
+    /// Applied on the type level, enforces this rule for all members (fields and properties)
     /// </summary>
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Class | AttributeTargets.Struct)]
     public sealed class InitOnlyAttribute : Attribute { }
