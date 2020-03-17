18377d095a8a33bcbf8fb601725c6f01a7e7d23b
Cezary Piątek <cezarypiatek@live.com>
2020-03-17 19:19:25 +0100

    #2: Add CS8618 suppressor for members affected by InitOnly and InitRequired

---
+++ src/CSharpExtensions.Analyzers3/SyntaxHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace CSharpExtensions.Analyzers3
+{
+    internal static class SyntaxHelper
+    {
+        public static TExpected FindNearestContainer<TExpected>(SyntaxNode tokenParent) where TExpected : SyntaxNode
+        {
+            if (tokenParent == null)
+            {
+                return null;
+            }
+
+            if (tokenParent is TExpected t1)
+            {
+                return t1;
+            }
+            return FindNearestContainer<TExpected>(tokenParent.Parent);
+        }
+    }
+}
