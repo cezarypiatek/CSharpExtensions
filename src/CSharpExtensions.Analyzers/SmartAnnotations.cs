736d240057cae020114d598ffc90e197fd937baf
Cezary Piątek <cezarypiatek@live.com>
2020-03-19 21:15:32 +0100

    Enforce init for non-nullable references

--- src/CSharpExtensions.Analyzers/SmartAnnotations.cs
+++ src/CSharpExtensions.Analyzers/SmartAnnotations.cs
@@ -3,6 +3,7 @@ namespace CSharpExtensions.Analyzers
     public static class SmartAnnotations
     {
         public const string InitRequired = "SmartAnalyzers.CSharpExtensions.Annotations.InitRequiredAttribute";
+        public const string InitRequiredForNotNull = "SmartAnalyzers.CSharpExtensions.Annotations.InitRequiredForNotNullAttribute";
         public const string InitOnly = "SmartAnalyzers.CSharpExtensions.Annotations.InitOnlyAttribute";
     }
 }
