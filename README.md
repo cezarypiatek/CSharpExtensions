ae91047c08795c0b7ff4ab1c22f9824ab0c0f13d
Cezary Piątek <cezarypiatek@live.com>
2021-02-13 11:47:46 +0100

    #29: Add analyzer for detecting unused values

--- README.md
+++ README.md
@@ -15,3 +15,4 @@ Articles that explain implemented concepts:
 |CSE002|InitOnly member modification |`[InitOnly]`, `[InitOnlyOptional]`| [link](https://cezarypiatek.github.io/post/immutable-types-with-roslyn/#full-immutability) |
 |CSE003|Type should have the same fields as twin type| `[TwinType]` | [link](https://cezarypiatek.github.io/post/csharp-twin-types/#the-solution-extending-c-rules-with-custom-analyzer)|
 |CSE004| Member with InitOnlyOptional requires default value| `[InitOnlyOptional]` ||
+|CSE005| Return value unused | ||
---
+++ src/CSharpExtensions.Analyzers/ReturnValueUnusedAnalyzer.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CSharpExtensions.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ReturnValueUnusedAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "CSE005";
+        internal static readonly LocalizableString Title = "Return value unused";
+        internal static readonly LocalizableString MessageFormat = "Use the return value or discard it explicitly";
+        internal const string Category = "CSharp Extensions";
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            
+            context.RegisterSyntaxNodeAction(AnalyzeSyntax, SyntaxKind.InvocationExpression, SyntaxKind.AwaitExpression, SyntaxKind.ObjectCreationExpression);
+        }
+
+        private void AnalyzeSyntax(SyntaxNodeAnalysisContext obj)
+        {
+            if (obj.Node is ExpressionSyntax expression)
+            {
+                if (expression.Parent is ExpressionStatementSyntax && obj.SemanticModel.GetTypeInfo(expression).Type is { } type && type.SpecialType != SpecialType.System_Void)
+                {
+                    var diagnostic = Diagnostic.Create(Rule, expression.GetLocation());
+                    obj.ReportDiagnostic(diagnostic);
+                }
+            }
+        }
+    }
+}
