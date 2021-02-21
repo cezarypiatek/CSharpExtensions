4786991b47c1ca56cf9d90976d379b0b855d1890
Cezary Piątek <cezarypiatek@live.com>
2021-02-21 15:06:41 +0100

    #33:  Add scoring calculator for method invocation complexity

--- src/CSharpExtensions.Analyzers/ExpressionTooComplexAnalyzer.cs
+++ src/CSharpExtensions.Analyzers/ExpressionTooComplexAnalyzer.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using System.Collections.Immutable;
+using System.Linq.Expressions;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
@@ -62,8 +64,61 @@ namespace CSharpExtensions.Analyzers
             }
         }
 
+        class ExpressionScoringCalculator
+        {
+            public static int GetScore(ExpressionSyntax expression)
+            {
+                if (expression is InvocationExpressionSyntax or MemberAccessExpressionSyntax)
+                {
+                    var diameterScore = 0;
+                    var diameterLevel = 0;
+                    SyntaxNode currentElement = expression;
+                    while (currentElement is not null and not ArgumentSyntax)
+                    {
+                        diameterScore += currentElement switch
+                        {
+                            InvocationExpressionSyntax invocation when invocation.Expression is IdentifierNameSyntax identifierNameSyntax && identifierNameSyntax.Identifier.Text is "nameof" or "typeof" => 0,
+                            InvocationExpressionSyntax invocation  => diameterLevel == 0 ? invocation.ArgumentList.Arguments.Count : 2,  //TODO: Lambda argument should have higher score as well as object creation or method invocation
+                            MemberAccessExpressionSyntax => diameterLevel == 0 ? 0 : 1,
+                            _ => 0
+                        };
+                        currentElement = currentElement.Parent;
+                        diameterLevel++;
+                    }
+
+                    return diameterScore;
+                }
+
+                return expression switch
+                {
+                    ObjectCreationExpressionSyntax objectCreation => objectCreation.ArgumentList.Arguments.Count,
+                    AwaitExpressionSyntax => 2,
+                    ConditionalExpressionSyntax => 3,
+                    BinaryExpressionSyntax binaryExpression => GetScoreForBinary(binaryExpression),
+                    _ => 0
+                };
+            }
+
+            private static int GetScoreForBinary(BinaryExpressionSyntax binaryExpression)
+            {
+                if (binaryExpression.Left is InvocationExpressionSyntax or ObjectCreationExpressionSyntax or AwaitExpressionSyntax or ConditionalExpressionSyntax or BinaryExpressionSyntax or ParenthesizedExpressionSyntax)
+                {
+                    return 2;
+                }
+
+                if (binaryExpression.Right is InvocationExpressionSyntax or ObjectCreationExpressionSyntax or AwaitExpressionSyntax or ConditionalExpressionSyntax or BinaryExpressionSyntax or ParenthesizedExpressionSyntax)
+                {
+                    return 2;
+                }
+
+                return 1;
+            }
+        }
+
         public static bool IsNonTrivialExpression(ArgumentSyntax argument)
         {
+            return ExpressionScoringCalculator.GetScore(argument.Expression) > 1;
+
             if (argument.Expression is  InvocationExpressionSyntax invocationExpression)
             {
                 if (invocationExpression.Expression is IdentifierNameSyntax identifierNameSyntax && identifierNameSyntax.Identifier.Text is "nameof" or "typeof")
