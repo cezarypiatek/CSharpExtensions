4a0493584d2d8a95aa44ec5a889e1ab1c8201367
Jeroen van Warmerdam <jvanwarmerdam@healthcloudinitiative.com>
2023-12-06 13:28:03 +0100

    Add fixer

--- src/CSharpExtensions.Analyzers/AddMissingMembersOfTwinTypeCodeFixProvider.cs
+++ src/CSharpExtensions.Analyzers/AddMissingMembersOfTwinTypeCodeFixProvider.cs
@@ -14,19 +14,19 @@ using Microsoft.CodeAnalysis.Formatting;
 namespace CSharpExtensions.Analyzers
 {
     [ExportCodeFixProvider(LanguageNames.CSharp)]
-    public class AddMissingMembersOfTwinTypeCodeFixProvider: CodeFixProvider
+    public class AddMissingMembersOfTwinTypeCodeFixProvider : CodeFixProvider
     {
         public override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
             var semanticModel = await context.Document.GetSemanticModelAsync(context.CancellationToken);
             var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
             var typeDeclaration = root.FindNode(context.Span).FirstAncestorOrSelf<BaseTypeDeclarationSyntax>();
-            
-            if (typeDeclaration is {} && ModelExtensions.GetDeclaredSymbol(semanticModel, typeDeclaration) is INamedTypeSymbol namedType)
+
+            if (typeDeclaration is { } && ModelExtensions.GetDeclaredSymbol(semanticModel, typeDeclaration) is INamedTypeSymbol namedType)
             {
                 var twinTypes = SymbolHelper.GetTwinTypes(namedType).GroupBy(x => x.Type.ToDisplayString())
                     .ToDictionary(x => x.Key, x => x.ToList());
-                
+
                 foreach (var diagnostic in context.Diagnostics)
                 {
                     if (diagnostic.Id == TwinTypeAnalyzer.DiagnosticId && diagnostic.Properties.TryGetValue("TwinType", out var twinTypeName))
@@ -39,7 +39,7 @@ namespace CSharpExtensions.Analyzers
                                     ? $"Add missing members from type {twinTypeName}"
                                     : $"Add missing members from type {twinTypeName} prefixed with {twinTypeInfo.NamePrefix}";
 
-                                context.RegisterCodeFix(CodeAction.Create(message, token =>  AddMissingMembers(context.Document, namedType, typeDeclaration, twinTypeInfo, token)), diagnostic);
+                                context.RegisterCodeFix(CodeAction.Create(message, token => AddMissingMembers(context.Document, namedType, typeDeclaration, twinTypeInfo, token)), diagnostic);
                             }
                         }
                     }
@@ -50,16 +50,33 @@ namespace CSharpExtensions.Analyzers
         private async Task<Document> AddMissingMembers(Document contextDocument, INamedTypeSymbol namedType, BaseTypeDeclarationSyntax typeDeclaration, TwinTypeInfo twinTypeInfo, CancellationToken token)
         {
             var syntaxGenerator = SyntaxGenerator.GetGenerator(contextDocument);
-            var newMembers =  CreateMissingMembers(namedType, twinTypeInfo, syntaxGenerator).ToArray();
             var newType = typeDeclaration switch
             {
-                TypeDeclarationSyntax td => td.AddMembers(newMembers),
-                EnumDeclarationSyntax ed => ed.AddMembers(newMembers.OfType<EnumMemberDeclarationSyntax>().ToArray()),
+                TypeDeclarationSyntax td => AddMembers(td, namedType, twinTypeInfo, syntaxGenerator),
+                EnumDeclarationSyntax ed => AddEnumMembers(ed, namedType, twinTypeInfo, syntaxGenerator),
                 _ => typeDeclaration
             };
             return await ReplaceNodes(contextDocument, typeDeclaration, newType, token);
         }
 
+        private static TypeDeclarationSyntax AddMembers(TypeDeclarationSyntax td, INamedTypeSymbol namedType, TwinTypeInfo twinTypeInfo, SyntaxGenerator syntaxGenerator)
+        {
+            var newMembers = CreateMissingMembers(namedType, twinTypeInfo, syntaxGenerator).ToArray();
+            return td.AddMembers(newMembers);
+        }
+
+        private static EnumDeclarationSyntax AddEnumMembers(EnumDeclarationSyntax ed, INamedTypeSymbol namedType, TwinTypeInfo twinTypeInfo, SyntaxGenerator syntaxGenerator)
+        {
+            var members = new List<EnumMemberDeclarationSyntax>();
+            var twinMembers = twinTypeInfo.GetTwinMembersFor(namedType);
+            foreach (var twinMember in twinMembers)
+            {
+                var enumMember = (EnumMemberDeclarationSyntax)syntaxGenerator.EnumMember(twinMember.Symbol.Name).WithAdditionalAnnotations(Formatter.Annotation);
+                members.Add(enumMember);
+            }
+            var newMembers = SyntaxFactory.SeparatedList(members);
+            return ed.WithMembers(newMembers);
+        }
 
         public static async Task<Document> ReplaceNodes(Document document, SyntaxNode oldNode, SyntaxNode newNode, CancellationToken cancellationToken)
         {
@@ -70,7 +87,7 @@ namespace CSharpExtensions.Analyzers
 
         private static IEnumerable<MemberDeclarationSyntax> CreateMissingMembers(INamedTypeSymbol namedType, TwinTypeInfo twinTypeInfo, SyntaxGenerator syntaxGenerator)
         {
-            foreach (var missingMember in twinTypeInfo.GetMissingMembersFor(namedType).OrderBy(x=>x.Symbol.Name))
+            foreach (var missingMember in twinTypeInfo.GetMissingMembersFor(namedType).OrderBy(x => x.Symbol.Name))
             {
                 if (missingMember.Symbol is IPropertySymbol propertySymbol)
                 {
@@ -78,18 +95,14 @@ namespace CSharpExtensions.Analyzers
                 }
                 else if (missingMember.Symbol is IFieldSymbol fieldSymbol)
                 {
-                    if (namedType.TypeKind == TypeKind.Enum)
-                    {
-                        yield return (EnumMemberDeclarationSyntax)syntaxGenerator.EnumMember(fieldSymbol.Name).WithAdditionalAnnotations(Formatter.Annotation);
-                    }
-                    else yield return CreateAutoProperty(syntaxGenerator, missingMember.ExpectedName, fieldSymbol.Type);
+                    yield return CreateAutoProperty(syntaxGenerator, missingMember.ExpectedName, fieldSymbol.Type);
                 }
             }
         }
 
         private static PropertyDeclarationSyntax CreateAutoProperty(SyntaxGenerator syntaxGenerator, string name, ITypeSymbol type)
         {
-            var newProperty = (PropertyDeclarationSyntax) syntaxGenerator.PropertyDeclaration(name, syntaxGenerator.TypeExpression(type), Accessibility.Public);
+            var newProperty = (PropertyDeclarationSyntax)syntaxGenerator.PropertyDeclaration(name, syntaxGenerator.TypeExpression(type), Accessibility.Public);
             return newProperty.WithAccessorList(SyntaxFactory.AccessorList(SyntaxFactory.List(
 
                 new[]
